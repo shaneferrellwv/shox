5cc3247cda12eb432f51e566a13f130564d434c9
Shane Ferrell <shaneferrellwv@gmail.com>
2023-02-06 16:52:27 -0500

    implemented factory and soem next funcationality

--- LexicalAnalysis/edu/ufl/cise/plcsp23/Scanner.java
+++ LexicalAnalysis/edu/ufl/cise/plcsp23/Scanner.java
@@ -14,8 +14,8 @@ public class Scanner implements IScanner
 {
     // class variables
     final String input;
-    final char[] inputChars;    //array containing input chars, terminated with extra char 0
-    private enum State 
+    final char[] inputChars;    // array containing input chars, terminated with extra char 0
+    private enum State          // internal states
     {
         START,
         HAVE_EQ,
@@ -34,13 +34,14 @@ public class Scanner implements IScanner
         ch = inputChars[pos];
     }
     
-    // @Override
-    // public Token next() throws LexicalException 
-    // {
+    @Override
+    public Token next() throws LexicalException 
+    {
+        this.State = START;
+        scanToken();
         
-        
-        // return ;
-    // }
+        return ;
+    }
     
     private Token scanToken() throws LexicalException 
     {
@@ -52,6 +53,12 @@ public class Scanner implements IScanner
             {
                 case  START -> 
                 {
+                    if (((int)ch >= 65 && (int)ch <= 90) || 
+                        ((int)ch >= 97 && (int)ch <= 122) ||
+                        (ch == '_'))
+                    {
+                        state = IN_IDENT;
+                    }
                     
                 }
                 case HAVE_EQ ->
