86bbccb4e36f4ee9af2ecf81a29a2467b031c029
Shane Ferrell <shaneferrellwv@gmail.com>
2023-02-07 12:45:50 -0500

    we wrote a ton more lol

--- LexicalAnalysis/edu/ufl/cise/plcsp23/Scanner.java
+++ LexicalAnalysis/edu/ufl/cise/plcsp23/Scanner.java
@@ -19,7 +19,13 @@ public class Scanner implements IScanner {
         HAVE_EQ,
         IN_IDENT,
         IN_NUM_LIT,
-        HAVE_AMP
+        HAVE_AMP,
+        HAVE_BAR,
+        HAVE_STAR,
+        HAVE_LT,
+        HAVE_GT,
+        HAVE_LTMINUS,
+        IN_COMMENT
     }
     int pos; //position of ch
     char ch; //next char, ch == inputChars[pos]
@@ -58,26 +64,97 @@ public class Scanner implements IScanner {
                             state = State.HAVE_AMP;
                             nextChar();
                         }
-                        
-                        
+                        case 124 -> { // |
+                            state = State.HAVE_BAR;
+                            nextChar();
+                        }
+                        case 42 -> { // *
+                            state = State.HAVE_STAR;
+                            nextChar();
+                        }
+                        case 61 -> { // =
+                            state = State.HAVE_EQ;
+                            nextChar();
+                        }
+                        case 60 -> { // <
+                            state = State.HAVE_LT;
+                            nextChar();
+                        }
+                        case 62 -> { // >
+                            state = State.HAVE_GT;
+                            nextChar();
+                        }
                         case 46 -> { // .
+                            nextChar();
                             return new Token(IToken.Kind.DOT, tokenStart, pos - tokenStart, inputChars);
                         }
                         case 44 -> { // ,
+                            nextChar();
                             return new Token(IToken.Kind.COMMA, tokenStart, pos - tokenStart, inputChars);
                         }
                         case 63 -> { // ?
+                            nextChar();
                             return new Token(IToken.Kind.QUESTION, tokenStart, pos - tokenStart, inputChars);
                         }
                         case 58 -> { // :
+                            nextChar();
                             return new Token(IToken.Kind.COLON, tokenStart, pos - tokenStart, inputChars);
                         }
                         case 40 -> { // (
+                            nextChar();
                             return new Token(IToken.Kind.LPAREN, tokenStart, pos - tokenStart, inputChars);
                         }
                         case 41 -> { // )
+                            nextChar();
                             return new Token(IToken.Kind.RPAREN, tokenStart, pos - tokenStart, inputChars);
                         }
+                        case 91 -> { // [
+                            nextChar();
+                            return new Token(IToken.Kind.LSQUARE,tokenStart, pos - tokenStart, inputChars);
+                        }
+                        case 93 -> { // ]
+                            nextChar();
+                            return new Token(IToken.Kind.RSQUARE,tokenStart, pos - tokenStart, inputChars);
+                        }
+                        case 123 -> { // {
+                            nextChar();
+                            return new Token(IToken.Kind.LCURLY,tokenStart, pos - tokenStart, inputChars);
+                        }
+                        case 125 -> { // }
+                            nextChar();
+                            return new Token(IToken.Kind.RCURLY,tokenStart, pos - tokenStart, inputChars);
+                        }
+                        case 33 -> { // !
+                            nextChar();
+                            return new Token(IToken.Kind.BANG,tokenStart, pos - tokenStart, inputChars);
+                        }
+                        case 43 -> { // +
+                            nextChar();
+                            return new Token(IToken.Kind.PLUS,tokenStart, pos - tokenStart, inputChars);
+                        }
+                        case 45 -> { // -
+                            nextChar();
+                            return new Token(IToken.Kind.MINUS,tokenStart, pos - tokenStart, inputChars);
+                        }
+                        case 47 -> { // /
+                            nextChar();
+                            return new Token(IToken.Kind.DIV,tokenStart, pos - tokenStart, inputChars);
+                        }
+                        case 37 -> { // %
+                            nextChar();
+                            return new Token(IToken.Kind.MOD,tokenStart, pos - tokenStart, inputChars);
+                        }
+                        
+                        case 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
+                            76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86,
+                            87, 88, 89, 90, // A..Z
+                            97, 98, 99, 100, 101, 102, 103, 104, 105, 106,
+                            107, 108, 109, 110, 111, 112, 113, 114, 115, 
+                            116, 117, 118, 119, 120, 121, 122, // a..z
+                            95 -> {// _ 
+                                
+                        }
+                            
                         
                         default -> {
                             throw new UnsupportedOperationException(
@@ -85,9 +162,6 @@ public class Scanner implements IScanner {
                         }
                     }
                     
-                }
-                case HAVE_EQ -> {
-                
                 }
                 case IN_NUM_LIT -> {
                     
@@ -104,6 +178,66 @@ public class Scanner implements IScanner {
                     else
                         return new Token(IToken.Kind.BITAND, tokenStart, pos - tokenStart, inputChars);
                 }
+                case HAVE_BAR -> {
+                    if (ch == '|'){
+                        state = State.START;
+                        nextChar();
+                        return new Token(IToken.Kind.OR, tokenStart, pos - tokenStart, inputChars);
+                    }
+                    else
+                        return new Token(IToken.Kind.BITOR, tokenStart, pos - tokenStart, inputChars);
+                }
+                case HAVE_STAR -> {
+                    if (ch == '*'){
+                        state = State.START;
+                        nextChar();
+                        return new Token(IToken.Kind.EXP, tokenStart, pos - tokenStart, inputChars);
+                    }
+                    else
+                        return new Token(IToken.Kind.TIMES, tokenStart, pos - tokenStart, inputChars);
+                }
+                case HAVE_EQ -> {
+                    if (ch == '='){
+                        state = State.START;
+                        nextChar();
+                        return new Token(IToken.Kind.EQ, tokenStart, pos - tokenStart, inputChars);
+                    }
+                    else
+                        return new Token(IToken.Kind.ASSIGN, tokenStart, pos - tokenStart, inputChars);
+                }
+                case HAVE_LT -> {
+                    if (ch == '='){
+                        state = State.START;
+                        nextChar();
+                        return new Token(IToken.Kind.LE, tokenStart, pos - tokenStart, inputChars);
+                    }
+                    else if (ch == '-') {
+                        state = State.HAVE_LTMINUS;
+                        nextChar();
+                    }
+                    else
+                        return new Token(IToken.Kind.LT, tokenStart, pos - tokenStart, inputChars);
+                }
+                case HAVE_LTMINUS -> {
+                    if (ch == '>'){
+                        state = State.START;
+                        nextChar();
+                        return new Token(IToken.Kind.EXCHANGE, tokenStart, pos - tokenStart, inputChars);
+                    }
+                    else {
+                        lastChar();
+                        return new Token(IToken.Kind.LT, tokenStart, 1, inputChars);
+                    } 
+                }
+                case HAVE_GT -> {
+                    if (ch == '='){
+                        state = State.START;
+                        nextChar();
+                        return new Token(IToken.Kind.GE, tokenStart, pos - tokenStart, inputChars);
+                    }
+                    else
+                        return new Token(IToken.Kind.GT, tokenStart, pos - tokenStart, inputChars);
+                }
                 default ->  {
                     throw new UnsupportedOperationException("Bug in Scanner");  
                 }
@@ -115,6 +249,11 @@ public class Scanner implements IScanner {
     void nextChar() {
         ch = inputChars[++pos];
         column++;
+    }
+    
+    void lastChar() {
+        ch = inputChars[--pos];
+        column--;
     }
     
     void nextLineChar() {
--- LexicalAnalysis/edu/ufl/cise/plcsp23/TestScanner.java
+++ LexicalAnalysis/edu/ufl/cise/plcsp23/TestScanner.java
@@ -2,53 +2,169 @@ package edu.ufl.cise.plcsp23;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import edu.ufl.cise.plcsp23.IToken.Kind;
-class TestScanner {	
+class TestScanner {    
 
-	//check that this token is the EOF token
-	void checkEOF(IToken t) {
-	    assertEquals(Kind.EOF, t.getKind());
-	}
-	
-	void checkToken(Kind expectedKind, IToken t) {
-            assertEquals(expectedKind, t.getKind());
-        }       
+    //check that this token is the EOF token
+    void checkEOF(IToken t) {
+        assertEquals(Kind.EOF, t.getKind());
+    }
     
-
-	@Test
-	void emptyProg() throws LexicalException{
-	    String input = "";
-	    IScanner scanner = CompilerComponentFactory.makeScanner(input);
-	    checkEOF(scanner.next());
-	}
-	
-	@Test
-        void onlyWhiteSpace() throws LexicalException{
-            String input = " \t \r\n \f \n";
-            IScanner scanner = CompilerComponentFactory.makeScanner(input);
-            checkEOF(scanner.next()); 
-        }
+    void checkToken(Kind expectedKind, IToken t) {
+            assertEquals(expectedKind, t.getKind());
+        }     
         
-        @Test
-        void onlyBitAnd() throws LexicalException{
-            String input = "&";
-            IScanner scanner =
-            CompilerComponentFactory.makeScanner(input);
-            checkToken(Kind.BITAND,scanner.next());
-            checkEOF(scanner.next()); 
-        }
-        
-        @Test
-        void onlyAnd() throws LexicalException{
-            String input = "&&";
-            IScanner scanner =
-            CompilerComponentFactory.makeScanner(input);
-            checkToken(Kind.AND,scanner.next());
-            checkEOF(scanner.next()); 
-        }
-
     @Test
     public void test0()
     {
+    }
+
+    @Test
+    void emptyProg() throws LexicalException{
+        String input = "";
+        IScanner scanner = CompilerComponentFactory.makeScanner(input);
+        checkEOF(scanner.next());
+    }
+    
+    @Test
+    void onlyWhiteSpace() throws LexicalException{
+        String input = " \t \r\n \f \n";
+        IScanner scanner = CompilerComponentFactory.makeScanner(input);
+        checkEOF(scanner.next()); 
+    }
+        
+    @Test
+    void onlyBitAnd() throws LexicalException{
+        String input = "&";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.BITAND,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void onlyAnd() throws LexicalException{
+        String input = "&&";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.AND,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void andWithSpaces() throws LexicalException{
+        String input = "  && ";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.AND,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+
+    @Test
+    void onlyTwoDots() throws LexicalException{
+        String input = "..";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.DOT,scanner.next());
+        checkToken(Kind.DOT,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void onlyDotComma() throws LexicalException{
+        String input = ".,";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.DOT,scanner.next());
+        checkToken(Kind.COMMA,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void onlyDotCommaAnd() throws LexicalException{
+        String input = ".,&&";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.DOT,scanner.next());
+        checkToken(Kind.COMMA,scanner.next());
+        checkToken(Kind.AND,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void onlyDotBitandComma() throws LexicalException{
+        String input = ".&,";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.DOT,scanner.next());
+        checkToken(Kind.BITAND,scanner.next());
+        checkToken(Kind.COMMA,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void onlyExchange() throws LexicalException{
+        String input = "<->";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.EXCHANGE,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void onlyLessThanMinus() throws LexicalException{
+        String input = "<-";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.LT,scanner.next());
+        checkToken(Kind.MINUS,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void onlyLessThanMinusSpaceGreaterThan() throws LexicalException{
+        String input = "<- >";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.LT,scanner.next());
+        checkToken(Kind.MINUS,scanner.next());
+        checkToken(Kind.GT,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void onlyTimesNewLineExponent() throws LexicalException{
+        String input = "*\n**";
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.TIMES,scanner.next());
+        checkToken(Kind.EXP,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void onlyTimesCarriageReturnTimes() throws LexicalException{
+        String input = """
+        *
+        *
+        """;
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.TIMES,scanner.next());
+        checkToken(Kind.TIMES,scanner.next());
+        checkEOF(scanner.next()); 
+    }
+    
+    @Test
+    void onlyTimesCarriageReturnExponent() throws LexicalException{
+        String input = """
+        *
+        **
+        """;
+        IScanner scanner =
+        CompilerComponentFactory.makeScanner(input);
+        checkToken(Kind.TIMES,scanner.next());
+        checkToken(Kind.EXP,scanner.next());
+        checkEOF(scanner.next()); 
     }
 }
 
