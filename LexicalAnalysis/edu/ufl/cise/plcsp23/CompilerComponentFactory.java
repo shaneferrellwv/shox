eb5c4f09a8921e4a4d757bbf836a5edc0671916d
Shane Ferrell <shaneferrellwv@gmail.com>
2023-03-07 11:34:18 -0500

    added assignemnt 3 test class

--- LexicalAnalysis/edu/ufl/cise/plcsp23/CompilerComponentFactory.java
+++ LexicalAnalysis/edu/ufl/cise/plcsp23/CompilerComponentFactory.java
@@ -29,4 +29,16 @@ public class CompilerComponentFactory {
         } while (t.getKind() != IToken.Kind.EOF);
         return new Parser(tokens);
     }
+    
+    public static IParser makeParser(String input) throws LexicalException {
+        //add code to create a scanner and parser and return the parser.
+        IScanner scanner = new Scanner(input);
+        List<IToken> tokens = new ArrayList();
+        IToken t;
+        do {
+            t = scanner.next();
+            tokens.add(t);
+        } while (t.getKind() != IToken.Kind.EOF);
+        return new Parser(tokens);
+    }
 }
